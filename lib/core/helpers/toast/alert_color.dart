cdc7a55f44ec082954fe883b9a142b8f2e18b618
Huseyin Bedir <57250424+huseyinbedir@users.noreply.github.com>
2023-12-18 18:51:07 +0300

    Update alert_color.dart

---
+++ lib/core/helpers/toast/alert_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AlertColor {
+  static Color dangerColor = const Color(0xffc0392b);
+  static Color warningColor = const Color(0xfff39c12);
+  static Color infoColor = const Color(0xff2980b9);
+  static Color successColor = const Color(0xff27ae60);
+}
